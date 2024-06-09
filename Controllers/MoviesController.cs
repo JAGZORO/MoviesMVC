7e27b590f975a5dd7b94de0ec9fa9a552744fb02
JAGZORO <145173372+JAGZORO@users.noreply.github.com>
2024-06-09 17:58:03 -0300

    Semana4

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesMVC.Models;
+
+namespace MoviesMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //Simulación de creación de un objeto (model)
+            //Mas adelante vamos a ver como usar una base de datos
+            var movies = new Movies
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+
+
+            return View(movies);
+        }
+    }
+}

aef5e75024fb9b2cfa2df61d24fdb5dbf46dc398
JAGZORO <145173372+JAGZORO@users.noreply.github.com>
2024-06-09 16:26:02 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MoviesMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/ Reemplazar el método index:
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        //
+        // GET: /HelloWorld/Welcome/ Agregar este método:
+
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
